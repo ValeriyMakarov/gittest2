ee192c01d3e10eb1a64f596d10b20454afef5b1e
ValeriyMakarov <misteranon.com@gmail.com>
2021-08-20 20:29:26 +0300

    new line

--- src/Program.java
+++ src/Program.java
@@ -2,5 +2,6 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("!");
         System.out.println();
+        for (int i=0; i<2; i++) System.out.print(i);
     }
 }
